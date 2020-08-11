9e21f51efd95cb049e8a401cb7f111cc21c14308
buchwasa <17461354+buchwasa@users.noreply.github.com>
2020-08-11 02:06:36 -0400

    Remove client blacklist

--- src/server.rs
+++ src/server.rs
@@ -17,8 +17,6 @@ pub struct Server {
     pub start: SystemTime,
     /// Server socket
     pub sock: Option<UdpSocket>,
-    /// list of blocked users <address, blocked until>
-    pub blacklist: HashMap<String, SystemTime>,
 }
 
 impl Server {
@@ -26,10 +24,9 @@ impl Server {
         Server {
             server_id: random::<u64>(),
             clients: HashMap::default(),
-            bind_to: bind_to,
+            bind_to,
             start: SystemTime::now(),
             sock: None,
-            blacklist: HashMap::default(),
         }
     }
 
@@ -41,35 +38,7 @@ impl Server {
         );
         loop {
             let (len, src) = self.sock.as_ref().unwrap().recv_from(&mut buff).unwrap();
-            if self.is_blocked(src) {
-                info!(
-                    "Client {} sent a packet, but it was ignored because they're blocked!",
-                    src.ip().to_string()
-                );
-                continue;
-            }
             self.handle_packet(&buff[0..len], src);
-        }
-    }
-
-    fn block_addr(&mut self, to_block: String, time_in_seconds: u64) {
-        self.blacklist.insert(
-            to_block,
-            SystemTime::now().add(std::time::Duration::from_secs(time_in_seconds)),
-        );
-    }
-
-    fn is_blocked(&mut self, addr: SocketAddr) -> bool {
-        match self.blacklist.get(addr.ip().to_string().as_str()) {
-            Some(time) => {
-                if time.elapsed().is_err() {
-                    true
-                } else {
-                    self.blacklist.remove(addr.ip().to_string().as_str());
-                    false
-                }
-            }
-            None => false,
         }
     }
 }
